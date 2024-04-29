5ff9700cf0cb6902d4172536911aa3f908598cd9
Zsigmond Marias <zsigmondmarias@Zsigmonds-MacBook-Pro.local>
2024-04-29 14:52:20 +0200

    2024.04.29 óra anyaga - H12.00 csoport

--- Hetfo-12.00/sequelize/gyakorlat.md
+++ Hetfo-12.00/sequelize/gyakorlat.md
@@ -8,3 +8,34 @@ https://sequelize.org/docs/v6/core-concepts/model-querying-finders/
 
 https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#operators
 
+REST API - Category Model - CRUD műveletek
+CRUD: Create, Read (Single item, List), Update (whole object, fields), Delete
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+
+GET - Read - /categories //mind lekérése
+             /categories/:id //egy entitás lekérdezése
+
+POST - Create - /categories //egy új elem létrehozása
+
+PUT - Update - /categories/:id //egy elem teljes frissítése ID alapján
+PATCH - Update - /categories/:id //egy elem bizonyos mezőinek frissítése ID alapján
+
+DELETE - Delete - /categories //minden elem törlése
+DELETE - Delete - /categories/:id //egy elem törlése ID alapján
+
+API teszteléshez
+https://firecamp.io/download
+
+Fastify
+https://fastify.dev/
+https://www.npmjs.com/package/fastify
+
+https://json-schema.org/learn/getting-started-step-by-step
+https://fastify.dev/docs/latest/Reference/Validation-and-Serialization/
+
+JWT
+
+https://jwt.io/
+
+
+
--- Hetfo-12.00/sequelize/run/test.js
+++ Hetfo-12.00/sequelize/run/test.js
@@ -31,5 +31,75 @@ const { Op } = require("sequelize");
     // }));
 
 
-    console.log(await (await Post.findByPk(1)).getCategories());
+    // console.log(await (await Post.findByPk(1)).getCategories());
+
+    // console.log(
+    //     JSON.stringify(
+    //         await Post.findAll({
+    //             include: [
+    //                 {
+    //                     model: Category,
+    //                     // "as" tulajdonképpen nem kell, ha a models-ben nem adtunk meg alias-t
+    //                 },
+    //             ],
+    //         }),
+    //         // JSON.stringify testreszabása
+    //         null,
+    //         4
+    //     )
+    // );
+
+
+    // Ugyanez, picit jobban testreszabva:
+    // console.log(
+    //     JSON.stringify(
+    //         await Post.findAll({
+    //             // A bejegyzésből csak az id, title és text mezők jelenjenek meg
+    //             attributes: ["id", "title", "text"],
+    //             // És a bejegyzés tartalmazza még...
+    //             include: [
+    //                 {
+    //                     // ... a kategória modelt ...
+    //                     model: Category,
+    //                     // ... mint "Categories" alias ...
+    //                     as: "Categories",
+    //                     // ... és ezeket a mezőit kérje le:
+    //                     attributes: ["id", "name"],
+
+    //                     // Ez pedig azért kell, hogy a kapcsolótáblát ne szemetelje bele,
+    //                     // a legjobb ha kikommentezed az alábbi sort és megnézed, mi változik
+    //                     through: { attributes: [] },
+    //                 },
+    //             ],
+    //         }),
+    //         // JSON.stringify testreszabása
+    //         null,
+    //         4
+    //     )
+    // );
+
+    console.log(
+        JSON.stringify(
+            await Post.findAll({
+                // A bejegyzés minden mezőjét lekérjük, kivéve a timestamp-eket
+                attributes: {
+                    exclude: ["createdAt", "updatedAt"],
+                },
+                include: [
+                    {
+                        model: Category,
+                        // Megköveteljük, hogy a szülőhöz tartozzon a gyerek (Post-hoz a Category, hiszen a Post-ra hívtuk a findAll-t)
+                        required: true,
+                        // Továbbá megmondjuk, hogy a gyerekből (Category) semmilyen mezőt nem akarunk látni, csak a bejegyzésre vagyunk kíváncsiak
+                        attributes: [],
+                        //through: { attributes: [] },
+                    },
+                ],
+            }),
+            // JSON.stringify testreszabása
+            null,
+            4
+        )
+    );   
+
 }) ()
